15e462abecb62cdae818be8679a1897e7b198dac
iamnikhilgola <nikhil18129@iiitd.ac.in>
2019-09-29 15:38:56 +0530

    added specific room code

--- getspecificroom.php
+++ getspecificroom.php
@@ -18,22 +18,40 @@ $roomtype = $outp["RoomType"];
 $Sql1="";
 if($roomtype=='0'){
 	//Lecture Room
-	$Sql1 = "Select * from Room,LectureRoom,booking where Room.Roomid=LectureRoom.Room_Roomid and Booking.Room_Roomid=Room.Roomid";
+	$Sql1 = "Select * from Room,LectureRoom where Room.Roomid=LectureRoom.Room_Roomid";
 }
-else if ($roomtype=='1') {
+elseif ($roomtype=='1') {
 	//Discussion Room
-	$Sql1="Select * from Room,DiscussionRoom,booking where Room.Roomid=DiscussionRoom.Room_Roomid and Booking.Room_Roomid=Room.Roomid";
+	$Sql1="Select * from Room,DiscussionRoom where Room.Roomid=DiscussionRoom.Room_Roomid";
 }
-else if ($roomtype=='2') {
+elseif ($roomtype=='2') {
 	# code...
 	//MEETING ROOM
-	$Sql1="Select * from Room,MeetingRoom,booking where Room.Roomid=MeetingRoom.Room_Roomid and Booking.Room_Roomid=Room.Roomid";
+	$Sql1="Select * from Room,MeetingRoom where Room.Roomid=MeetingRoom.Room_Roomid";
 }
-else if($roomtype=='3'){
+elseif($roomtype=='3'){
 	$Sql1="Select * from faculty,faculty_office,Room where Room.Roomid=faculty_office.Roomid and faculty_office.facultyid = faculty.facultyid";
 	//faculty office	
 }
+elseif ($roomtype=='4') {
+	# code...
+	//Lab
+	$Sql1 ="Select * from Room,Lab where Room.Roomid = Lab.Room_Roomid";
 
+}
+elseif($roomtype=='5'){
+	$Sql1 = "Select * from Room,ResearchLabs,faculty where Room.Roomid = ResearchLabs.Room_Roomid and faculty.facultyid=ResearchLabs.faculty_facultyid";
+}
+else{
+	$Sql1="";
+}
+
+$stmt = $conn->prepare($Sql1);
+$stmt->execute();
+$result = $stmt->get_result();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+
+echo json_encode($outp);
 
 $conn->close();
 ?>
